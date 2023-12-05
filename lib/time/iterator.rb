e4ef1569a4949148ad734f8fdb8c98ce1e6e376a
Michael G. Schwern <schwern@pobox.com>
2023-12-04 20:46:39 -0800

    Copy the code and tests from email-integrator.
    This code was written by me on contract. I have permission to do this
    from the client.

--- lib/time/iterator.rb
+++ lib/time/iterator.rb
@@ -1,10 +1,81 @@
-# frozen_string_literal: true
-
-require_relative "iterator/version"
-
-module Time
-  module Iterator
-    class Error < StandardError; end
-    # Your code goes here...
+class Numeric
+  def quarter
+    (3 * self).months
   end
+  alias_method :quarters, :quarter
+end
+
+module EmailIntegrator
+  module Time
+    INFINITY = 1.0 / 0.0
+    PERIODS = [:second, :minute, :hour, :day, :week, :month, :quarter, :year].freeze
+    ITERATE_BY = PERIODS + PERIODS.map { |period| period.to_s.pluralize.to_sym }
+
+    private def valid_period?(period)
+      raise ArgumentError, "Unknown time period: #{period}" if PERIODS.exclude?(period)
+    end
+
+    private def method_for_period(method, period)
+      period = period.to_sym
+      valid_period?(period)
+      send("#{method}_#{period}".to_sym)
+    end
+
+    def beginning_of(period)
+      method_for_period(:beginning_of, period)
+    end
+
+    def end_of(period)
+      method_for_period(:end_of, period)
+    end
+
+    def iterate(by:, every: 1)
+      by = by.to_sym
+      if ITERATE_BY.exclude?(by)
+        raise ArgumentError, "Unknown period to iterate by: #{by}"
+      end
+
+      Enumerator.new do |block|
+        (0..INFINITY).each do |num|
+          block << (self + (num.send(by) * every))
+        end
+      end
+    end
+
+    def days_in_month
+      ::Time.days_in_month( month, year )
+    end
+
+    def days_in_year
+      ::Time.days_in_year( year )
+    end
+
+    # Creates a Range from the Time to the end of today
+    # @return [Range]
+    def span_to_end_of_today
+      beginning_of_next_day = ::Time.current.tomorrow.beginning_of_day
+      (self...beginning_of_next_day)
+    end
+
+    # @return [String] the abbreviated day of the week
+    def dow
+      strftime("%a")
+    end
+
+    def iso_date
+      strftime("%Y-%m-%d")
+    end
+
+    def human_date
+      strftime("%a %b %-d %Y")
+    end
+  end
+end
+
+class Time
+  include EmailIntegrator::Time
+end
+
+class ActiveSupport::TimeWithZone
+  include EmailIntegrator::Time
 end
--- spec/time/iterator_spec.rb
+++ spec/time/iterator_spec.rb
@@ -1,11 +1,137 @@
-# frozen_string_literal: true
+require 'rails_helper'
 
-RSpec.describe Time::Iterator do
-  it "has a version number" do
-    expect(Time::Iterator::VERSION).not_to be nil
+RSpec.describe Time do
+  let(:time) { described_class.current }
+
+  describe '#quarter(s)' do
+    [:quarter, :quarters].each do |method|
+      it 'adds 3 months per quarter' do
+        expect( time + 1.send(method) ).to eq time + 3.months
+        expect( time + 3.send(method) ).to eq time + 9.months
+        expect( time + 4.send(method) ).to eq time + 1.year
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'period check' do
+    it 'raises on invalid period' do
+      expect { time.beginning_of(:time) }.to raise_error(ArgumentError)
+      expect { time.end_of(:universe) }.to raise_error(ArgumentError)
+      expect { time.iterate(by: :colors) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#beginning_of' do
+    periods2methods = {
+      minute: :beginning_of_minute,
+      hour: :beginning_of_hour,
+      day: :beginning_of_day,
+      week: :beginning_of_week,
+      month: :beginning_of_month,
+      quarter: :beginning_of_quarter,
+      year: :beginning_of_year
+    }.freeze
+
+    periods2methods.each do |period, method|
+      it "handles #{period}" do
+        expect(time.beginning_of(period)).to eq time.send(method)
+      end
+    end
+  end
+
+  describe '#end_of' do
+    periods2methods = {
+      minute: :end_of_minute,
+      hour: :end_of_hour,
+      day: :end_of_day,
+      week: :end_of_week,
+      month: :end_of_month,
+      quarter: :end_of_quarter,
+      year: :end_of_year
+    }.freeze
+
+    periods2methods.each do |period, method|
+      it "handles #{period}" do
+        expect(time.end_of(period)).to eq time.send(method)
+      end
+    end
+  end
+
+  describe '#iterate' do
+    it 'uses an Enumerator' do
+      expect( time.iterate(by: :day) ).to be_a(Enumerator)
+    end
+
+    [ :second, :minute, :hour, :day, :week, :month, :quarter, :year,
+      :seconds, :minutes, :hours, :days, :weeks, :months, :quarter, :years
+    ].each do |period|
+      it "iterates by #{period}" do
+        expect(
+          time.iterate(by: period).take(3)
+        ).to eq [time, time + 1.send(period), time + 2.send(period)]
+      end
+    end
+
+    it 'iterates every X periods' do
+      expect(
+        time.iterate(by: :day, every: 3).take(3)
+      ).to eq [time, time + 3.days, time + 6.days]
+    end
+
+    it 'does not alter the original time' do
+      orig = time.clone
+      time.iterate(by: :day).take(5)
+      expect( time ).to eq orig
+    end
+  end
+
+  describe '#days_in_month/year' do
+    # Make sure it's accounting for a leap year
+    let(:time) {
+      described_class.current.change(year: 2020, month: 2, day: 5)
+    }
+
+    it 'gives the days in the current month' do
+      expect( time.days_in_month ).to eq 29
+      expect( time.days_in_year ).to eq 366
+    end
+  end
+
+  describe '#span_to_end_of_today', travel_to: described_class.current do
+    it 'creates a span to the end of today' do
+      start_span = 7.days.ago
+      span = start_span.span_to_end_of_today
+      end_span = described_class.current.tomorrow.beginning_of_day
+
+      expect( span.begin ).to eq start_span
+      expect( span.end ).to eq end_span
+      expect( span ).to be_exclude_end
+
+      # See https://github.com/rubocop-hq/rubocop-rspec/issues/926
+      expect( span.cover?(start_span) ).to be_truthy
+      expect( span.cover?(end_span) ).to be_falsey
+      expect( span.cover?(end_span - 1.second) ).to be_truthy
+    end
+  end
+
+  describe '#dow' do
+    it 'returns the abbreviated day of the week' do
+      wednesday = described_class.zone.local(2019, 9, 18)
+      expect( wednesday.dow ).to eq 'Wed'
+    end
+  end
+
+  describe '#iso_date' do
+    it 'returns an ISO date' do
+      time = described_class.zone.local(2019, 9, 8)
+      expect( time.iso_date ).to eq '2019-09-08'
+    end
+  end
+
+  describe '#human_date' do
+    it 'formats the date for humans' do
+      sunday = described_class.zone.local(2019, 9, 8)
+      expect( sunday.human_date ).to eq 'Sun Sep 8 2019'
+    end
   end
 end
