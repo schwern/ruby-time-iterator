0280d519c2170a9cd17ed403340fc5aa5ad00635
Michael G. Schwern <schwern@pobox.com>
2023-12-16 20:51:02 -0800

    Have the user pass in an ActiveSupport::Duration.
    Simplifies everything greatly. We need it anyway.
    
    We can do a version without ActiveSupport if folks want it.
    
    Also
    * Remove all the core overrides, it's rude for a gem to do that.
      They were for the app TimeIterator came from.
    * Remove `quarter`, a user can do a 3 month duration.

--- lib/time_iterator.rb
+++ lib/time_iterator.rb
@@ -1,32 +1,17 @@
 require "active_support"
-require "active_support/core_ext/integer/time"
-require_relative "time_iterator/core_ext/numeric"
-require_relative "time_iterator/core_ext/time"
+require "active_support/duration"
 
 # Time iteration.
 module TimeIterator
-  INFINITY = 1.0 / 0.0
-  PERIODS = {
-    second: :seconds,
-    minute: :minutes,
-    hour: :hours,
-    day: :days,
-    week: :weeks,
-    month: :months,
-    quarter: :quarters,
-    year: :years
-  }.freeze
-  ITERATE_BY = (PERIODS.keys + PERIODS.values).freeze
-
   class << self
-    def iterate(start, by:, every: 1)
-      by = by.to_sym
-
-      raise ArgumentError, "Unknown period to iterate by: #{by}" unless ITERATE_BY.include?(by)
+    def iterate(start, by:)
+      raise ArgumentError, "`by` must be an ActiveSupport::Duration" unless by.is_a?(ActiveSupport::Duration)
 
       Enumerator.new do |block|
-        (0..INFINITY).each do |num|
-          block << (start + (num.send(by) * every))
+        time = start
+        loop do |_num|
+          block << time
+          time += by
         end
       end
     end
--- lib/time_iterator/core_ext/numeric.rb
+++
@@ -1,7 +0,0 @@
-# Inject 3.quarters
-class Numeric
-  def quarter
-    (3 * self).months
-  end
-  alias quarters quarter
-end
